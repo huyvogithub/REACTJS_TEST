9f9f6a6e56052fee7937bc03d992760be4298ef3
HUYVO <huyvo0188630@gmail.com>
2023-12-17 09:06:03 +0700

    up load hide

--- src/components/UserList.jsx
+++ src/components/UserList.jsx
@@ -3,31 +3,74 @@ import axios from 'axios';
 
 const UserList = () => {
   const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-tqlme/endpoint/GET');
+      setUserData(response.data);
+      setError(null);
+    } catch (error) {
+      console.error('Lỗi khi lấy dữ liệu:', error);
+      setError('Không thể tải dữ liệu. Vui lòng thử lại sau.');
+    } finally {
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get('https://ap-southeast-1.aws.data.mongodb-api.com/app/data-tqlme/endpoint/GET');
-        setUserData(response.data);
-      } catch (error) {
-        console.error('Lỗi khi lấy dữ liệu:', error);
-      }
-    };
-
     fetchData();
   }, []);
 
+  const handleRefresh = () => {
+    setLoading(true);
+    fetchData();
+  };
+
+  const toggleShowPassword = () => {
+    setShowPassword((prevShowPassword) => !prevShowPassword);
+  };
+
   return (
     <div>
-      <h2>DANH SÁCH VÀ STT </h2>
-      <h5>(Refresh)</h5>
-      <ul>
-        {userData.map((user, index) => (
-          <li key={index}>
-            Tên người dùng: {user.username}, Mật khẩu: {user.password}
-          </li>
-        ))}
-      </ul>
+      <h2 style={{ fontSize: '1.5em' }}>DANH SÁCH VÀ STT </h2>
+      <div style={{ marginBottom: '10px' }}>
+        <button onClick={handleRefresh} disabled={loading}>
+          {loading ? 'Refreshing...' : 'Refresh'}
+        </button>
+        <button onClick={toggleShowPassword} style={{ marginLeft: '10px' }}>
+          {showPassword ? 'Ẩn' : 'Hiện'} Mật khẩu
+        </button>
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <table style={{ width: '100%', borderCollapse: 'collapse', marginTop: '10px' }}>
+        <thead>
+          <tr>
+            <th>STT</th>
+            <th>Tên người dùng</th>
+            <th>Mật khẩu</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userData.map((user, index) => (
+            <tr key={index}>
+              <td>{index + 1}</td>
+              <td style={{ fontSize: '1.2em' }}>{user.username}</td>
+              <td style={{ fontSize: '1.2em' }}>
+                {showPassword ? (
+                  user.password
+                ) : (
+                  <span style={{ cursor: 'pointer', fontSize: '1.2em' }} onClick={toggleShowPassword}>
+                    ••••••
+                  </span>
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 };
